2c0ff7fd35b5df6f525afb7f3f31f3316b504353
donovanxie <43017503+donovanxie@users.noreply.github.com>
2022-12-13 13:16:22 -0500

    Update LogContext.swift

--- KeepYourDistance/LogContext.swift
+++ KeepYourDistance/LogContext.swift
@@ -1,10 +1,3 @@
-//
-//  LogContext.swift
-//  KeepYourDistance
-//
-//  Created by Kraig Spear on 5/21/20.
-//  Copyright © 2020 spearware. All rights reserved.
-//
 
 import Foundation
 import os.log
