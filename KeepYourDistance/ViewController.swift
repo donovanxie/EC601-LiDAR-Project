5292ff3ec86a6b63ab147184ba3f6c75fcffd95b
donovanxie <43017503+donovanxie@users.noreply.github.com>
2022-12-13 13:17:11 -0500

    Update ViewController.swift

--- KeepYourDistance/ViewController.swift
+++ KeepYourDistance/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  KeepYourDistance
-//
-//  Created by Kraig Spear on 5/13/20.
-//  Copyright © 2020 spearware. All rights reserved.
-//
+
 
 import ARKit
 import AVFoundation
